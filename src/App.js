51bab1b67c225a59dd5ca8958af270fefa474579
Shubhamsg7 <shubhamkumar8028@gmail.com>
2020-11-12 01:26:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './Data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const Submit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 10) {
+      amount = 10;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <h1>lorem ipsum Generator</h1>
+      <form className='form' onSubmit={Submit}>
+        <label htmlFor='amount'>Paragraphs : </label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn'>Generate</button>
+      </form>
+      <article className='text'>
+        {text.map((item, index) => {
+          return <><p key={index}>{item}</p></>;
+        })}
+      </article>
+    </section>
   );
 }
 
